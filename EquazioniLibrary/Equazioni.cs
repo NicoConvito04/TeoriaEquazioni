ae32dc3528367b6d71503cb50396d4226d4faea7
Lucia Rossi <nicoconvito04@gmail.com>
2021-02-15 11:10:52 +0100

    Aggiungere i file di progetto.

---
+++ EquazioniLibrary/Equazioni.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EquazioniLibrary
+{
+    public class Equazioni
+    {
+        public static bool IsDetermined(double a, double b)
+        {
+            bool risposta = false;
+
+
+
+            return risposta;
+        }
+    }
+}
