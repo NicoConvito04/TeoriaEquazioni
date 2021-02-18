ceb85a66d63bc1dbf36532efc89b388d3e4fd6a8
Lucia Rossi <nicoconvito04@gmail.com>
2021-02-18 12:15:50 +0100

    Primo metodo

--- EquazioniLibrary.test/UnitTest1.cs
+++ EquazioniLibrary.test/UnitTest1.cs
@@ -9,6 +9,11 @@ namespace EquazioniLibrary.test
         [TestMethod]
         public void TestMethod1()
         {
+            double a = 0;
+            bool risposta_aspettata=true;
+            bool risposta_effettiva = Equazioni.IsDetermined(a);
+            Assert.AreEqual(risposta_aspettata, risposta_effettiva);
+
         }
     }
 }
--- EquazioniLibrary/Equazioni.cs
+++ EquazioniLibrary/Equazioni.cs
@@ -4,13 +4,20 @@ namespace EquazioniLibrary
 {
     public class Equazioni
     {
-        public static bool IsDetermined(double a, double b)
+        public static bool IsDetermined(double a)
         {
             bool risposta = false;
-
-
-            
+            if (a != 0)
+            {
+                risposta = false;
+            }
+            else
+            {
+                risposta = true;
+            }
             return risposta;
         }
+
+        public static bool IsInconsisted
     }
 }
