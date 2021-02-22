e49587ffb07e809570fa1c724705edf9c7734398
Lucia Rossi <nicoconvito04@gmail.com>
2021-02-22 11:10:39 +0100

    Quarto metodo

--- EquazioniLibrary.test/UnitTest1.cs
+++ EquazioniLibrary.test/UnitTest1.cs
@@ -29,9 +29,21 @@ namespace EquazioniLibrary.test
         [TestMethod]
         public void EquazioneSecondoGrado()
         {
-            double a = 0;
+            double a = 5;
             bool risposta_aspettata = true;
             bool risposta_effettiva = Equazioni.IsDegree2(a);
+            Assert.AreEqual(risposta_aspettata, risposta_effettiva);
+
+        }
+
+        [TestMethod]
+        public void EquazioneDelta()
+        {
+            double a = 5;
+            double b = 3;
+            double c = 2;
+            double risposta_aspettata = -31;
+            double risposta_effettiva = Equazioni.Delta(a,b,c);
             Assert.AreEqual(risposta_aspettata, risposta_effettiva);
 
         }
--- EquazioniLibrary/Equazioni.cs
+++ EquazioniLibrary/Equazioni.cs
@@ -41,5 +41,12 @@ namespace EquazioniLibrary
                 return true;
             }
         }
+        
+        public static double Delta(double a, double b, double c)
+        {
+            double delta = (b * b) - 4 * a * c;
+
+            return delta;
+        }
     }
 }
